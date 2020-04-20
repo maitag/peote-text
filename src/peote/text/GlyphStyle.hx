a07472e25422f4d47e7460d81c68fcdc6cf8bd71
Sylvio Sell <info@maitag.de>
2020-04-21 01:07:54 +0200

    arranging new ;)

---
+++ src/peote/text/GlyphStyle.hx
@@ -0,0 +1,14 @@
+package peote.text;
+
+import peote.view.Color;
+
+class GlyphStyle {
+	//@global public var color:Color = Color.GREEN;
+	public var color:Color = Color.GREEN;
+	
+	public var width:Float = 20.0;
+	public var height:Float = 20.0;
+	//public var scale:Float = 2.0;
+	
+	public function new() {}
+}
